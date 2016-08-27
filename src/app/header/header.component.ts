7dc119869e68df275156625d42f9c7383516b2ff
王裕凱 <kevin@simsix.com>
2016-08-27 11:22:49 +0800

    在 subtitle 右邊顯示 +1 並在點擊 subtitle 的時候會 +1，按下 Shift-Click 的時候會 -1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,9 @@ export class HeaderComponent implements OnInit {
 
   title = 'Angular 2 @ 2016/08/27';
   imgTitle = 'This is Will 保哥';
-  subTitle = '記載著<strong> Will </strong>在網路世界的學習心得與技術分享';
+  subTitle = '記載著 Will 在網路世界的學習心得與技術分享';
+num =1;
+
 
   constructor() { }
 
@@ -20,5 +22,16 @@ export class HeaderComponent implements OnInit {
 
     this.title = value;
 
+  }
+
+  plusOrMinusOne($event: MouseEvent){
+    //﹝判斷﹞shiftKey 是否被按下
+    if ($event.shiftKey) {
+      // num - 1
+      this.num -= 1;
+    }else {
+      // num + 1
+      this.num += 1;
+    }
   }
 }
